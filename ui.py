ca2dadefae1293ff892f1e37cd805f62ca1c8caf
Łukasz Malinowski <lukaszmalinowski14@gmail.com>
2023-10-24 23:45:45 +0200

    "ve2"

--- ui.py
+++ ui.py
@@ -5,6 +5,10 @@ THEME_COLOR = "#375362"
 
 
 class QuizInterface:
+
+    # def button_true_func():
+    #     QuizBrain.check_answer(self, "true")
+
     def __init__(self, quiz_brain: QuizBrain):
         self.quiz = quiz_brain
         self.question = "sfdfsdf"
@@ -29,20 +33,46 @@ class QuizInterface:
 
         # create buttons
         self.true_button = Button(image=true_img,
-                                  highlightthickness=0)
+                                  highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
         self.true_button.config(padx=20, pady=20)
 
         self.false_button = Button(image=false_img,
-                                   highlightthickness=0)
+                                   highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
         self.false_button.config(padx=20, pady=20)
 
         # wywalanie pierwzego pytania
-        get_next_question(self)
+        self.get_next_question()
         self.window.mainloop()
 
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(
+                self.question_text, text="You've reached the end of the questions")
+            self.false_button.config(state="disabled")
+            self.true_button.config(state="disabled")
 
-def get_next_question(self):
-    q_text = self.quiz.next_question()
-    self.canvas.itemconfig(self.question_text, text=q_text)
+    def true_pressed(self):
+        is_right = self.quiz.check_answer("true")
+        self.give_feedback(is_right)
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("false")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if (is_right):
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000, self.get_next_question)
+
+        # self.change_button_color('red')
+        # self.after(3000,lambda: self.change_button_color('black'))
